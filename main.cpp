a2ee3c8f4b686e40482319d0d59ebe9cdaf00481
Yemsheng <msheng.ye@foxmail.com>
2012-09-18 18:42:23 +0800

    first StringMatch

--- main.cpp
+++ main.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
+#include"StringMatch.h"
+
 int main()
 {
 	cout<<"hello"<<endl;
+
+	char *a = "qwdfaaa";
+	char *b = "qwtefaa";
+	char *c = "qaawdfa";
+	char *d = "qwe";
+	StringMatch sMatch;
+	bool matchFlag = false;
+	matchFlag = sMatch.StringMatch1(a,b);
+	cout<<a<<" "<<b<<" "<<matchFlag<<endl;
+	matchFlag = sMatch.StringMatch1(a,c);
+    cout<<a<<" "<<c<<" "<<matchFlag<<endl;
+	matchFlag = sMatch.StringMatch1(a,d);
+    cout<<a<<" "<<d<<" "<<matchFlag<<endl;
+
+	matchFlag = sMatch.StringMatch2(a,b);
+	cout<<a<<" "<<b<<" "<<matchFlag<<endl;
+	matchFlag = sMatch.StringMatch2(a,c);
+    cout<<a<<" "<<c<<" "<<matchFlag<<endl;
+	matchFlag = sMatch.StringMatch2(a,d);
+    cout<<a<<" "<<d<<" "<<matchFlag<<endl;
+
+
 	return 0;
 }
