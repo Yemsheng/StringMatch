c78a6ee5b95e7d69b1870aa89d20c45a3c37827e
Yemsheng <msheng.ye@foxmail.com>
2012-09-18 18:52:24 +0800

    cpp indentation

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	cout<<"hello"<<endl;
-
+	
 	char *a = "qwdfaaa";
 	char *b = "qwtefaa";
 	char *c = "qaawdfa";
@@ -20,14 +20,14 @@ int main()
     cout<<a<<" "<<c<<" "<<matchFlag<<endl;
 	matchFlag = sMatch.StringMatch1(a,d);
     cout<<a<<" "<<d<<" "<<matchFlag<<endl;
-
+	
 	matchFlag = sMatch.StringMatch2(a,b);
 	cout<<a<<" "<<b<<" "<<matchFlag<<endl;
 	matchFlag = sMatch.StringMatch2(a,c);
     cout<<a<<" "<<c<<" "<<matchFlag<<endl;
 	matchFlag = sMatch.StringMatch2(a,d);
-    cout<<a<<" "<<d<<" "<<matchFlag<<endl;
-
-
+	cout<<a<<" "<<d<<" "<<matchFlag<<endl;
+	
+	
 	return 0;
 }
